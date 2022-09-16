f1a8a48e6a800e55299b3e358e83ddce5d8ee633
sourcejs-dev <82123435+sourcejs-dev@users.noreply.github.com>
2022-09-16 14:29:27 +0300

    Update profile.controller.js

--- apps/library/src/modules/user/profile/profile.controller.js
+++ apps/library/src/modules/user/profile/profile.controller.js
@@ -1,4 +1,32 @@
+const { BookService } = require('@modules/book/book.service');
+
 const getPageProfile = (req, res) =>
 	res.render('user/profile', { title: 'Профиль', user: req.user });
 
-module.exports.UserProfileController = { getPageProfile };
+const getPageBooks = async (req, res) => {
+	const books = await BookService.findAll();
+	return res.render('book/index', { title: 'Книги', books });
+};
+
+const getPageBookById = async (req, res) => {
+	const { id } = req.params;
+	const book = await BookService.findById(id);
+	return res.render('book/view', { title: book.title, book });
+};
+
+const getPageUpdateBookById = async (req, res) => {
+	const { id } = req.params;
+	const book = await BookService.findById(id);
+	return res.render('book/update', { title: `Обновление ${book.title}`, book });
+};
+
+const getPageCreateBook = (req, res) =>
+	res.render('book/create', { title: 'Добавить книгу' });
+
+module.exports.UserProfileController = {
+	getPageProfile,
+	getPageBooks,
+	getPageBookById,
+	getPageUpdateBookById,
+	getPageCreateBook,
+};
