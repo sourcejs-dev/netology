22841adca06f83e2605123813e0fad03378fbc85
sourcejs-dev <82123435+sourcejs-dev@users.noreply.github.com>
2022-09-16 14:28:27 +0300

    add handle page-error

--- apps/library/src/middlewares/error.middleware.js
+++ apps/library/src/middlewares/error.middleware.js
@@ -1,7 +1,16 @@
 const ApiError = require('@modules/api-error');
+const PageError = require('@modules/page-error');
 
 exports.errorHandler = (err, req, res, next) => {
 	console.log(err);
+
+	if (err instanceof PageError) {
+		return res.status(err.status).render('layout/error', {
+			title: 'Ошибка',
+			url: req.headers.referer || '/',
+			message: err.message,
+		});
+	}
 
 	if (err instanceof ApiError) {
 		return res.status(err.status).json({ message: err.message });
